a98364c2bcc45bcb07f62c5e967ba4bdd430c1e9
Edixon Piña <edixonalbertto@gmail.com>
2023-03-26 21:15:37 -0400

    create component Icons, add styles in layout and page
    - add new variables in global.scss
    - create component general in /scss

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,122 @@
+<script lang="ts" setup>
+const route = useRoute()
+</script>
+
 <template>
-  <div>
-    <header>header</header>
+  <div class="app">
+    <header>
+      <div class="naming">
+        <Icons name="pinea" />
+        <h2>Edixon Piña</h2>
+      </div>
+    </header>
 
     <nav>
       <ul>
-        <li><NuxtLink to="/">Projects</NuxtLink></li>
+        <li :class="{ active: route.name === 'index' }">
+          <NuxtLink to="/">Projects</NuxtLink>
+          <div class="line"></div>
+        </li>
       </ul>
     </nav>
 
-    <slot />
+    <main>
+      <slot />
+    </main>
 
-    <footer>footer</footer>
+    <footer>
+      <div class="data">
+        <span>Creado por "Edixon Piña"</span>
+        <a class="p-link" href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a class="p-link" href="#" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </div>
+      <div class="tools">
+        <a class="p-link" href="#" target="_blank" rel="noopener noreferrer">Nuxt3</a>
+        <a class="p-link" href="#" target="_blank" rel="noopener noreferrer">TypeScript</a>
+        <a class="p-link" href="#" target="_blank" rel="noopener noreferrer">GitHubAction</a>
+        <a class="p-link" href="#" target="_blank" rel="noopener noreferrer">GitHubPage</a>
+      </div>
+    </footer>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.app {
+  width: calc(100vw);
+  height: 100vh;
+  padding: 0 340px;
+
+  header {
+    width: 100%;
+    height: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .naming {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      h2 {
+        font-size: 1.5rem;
+      }
+    }
+  }
+
+  nav {
+    width: 100%;
+
+    ul {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      li {
+        width: max-content;
+
+        a {
+          color: #000;
+          text-decoration: none;
+          font-size: 1.25rem;
+        }
+
+        .line {
+          height: 3px;
+          margin-top: 10px;
+        }
+
+        &.active {
+          a {
+            font-weight: 600;
+          }
+          .line {
+            background: $color-primary;
+          }
+        }
+      }
+    }
+  }
+
+  footer {
+    width: calc(100% - 340px * 2);
+    height: 160px;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+    align-items: center;
+    justify-content: center;
+
+    .data,
+    .tools {
+      span,
+      a {
+        margin: 0 25px;
+        font-size: 1.25rem;
+      }
+    }
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,120 @@
 <template>
-  <div>
+  <div class="projects">
     <h1>Proyectos</h1>
+
+    <div class="container">
+      <div class="project-card">
+        <div class="header">
+          <span>2022</span>
+
+          <div class="star">
+            <span>10</span>
+            <Icons name="star" />
+          </div>
+        </div>
+
+        <h4 class="title">Project Name</h4>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+        <div class="badges">
+          <a class="badge" href="#" target="_blank" rel="noopener noreferrer">
+            <Icons name="github" />
+            <span>Código</span>
+          </a>
+
+          <div class="badge">
+            <Icons name="browser" />
+            <span>Demo</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.projects {
+  width: 100%;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  .container {
+    width: 100%;
+
+    .project-card {
+      width: 400px;
+      height: 210px;
+      padding: 25px;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      row-gap: 14px;
+      background: #fff;
+      border: 1px solid $color-grey-lite;
+      border-radius: 6px;
+
+      .header {
+        user-select: none;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .star {
+          display: flex;
+          align-items: center;
+          justify-content: flex-start;
+          column-gap: 5px;
+
+          .p-icon {
+            width: 24px;
+          }
+        }
+      }
+
+      h4 {
+        margin: 0;
+        font-size: 1rem;
+      }
+
+      p {
+        margin: 0;
+        font-size: 0.875rem;
+      }
+
+      .badges {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        column-gap: 20px;
+
+        .badge {
+          cursor: pointer;
+          user-select: none;
+          width: 100px;
+          padding: 0 5px;
+          display: flex;
+          align-items: center;
+          justify-content: flex-start;
+          column-gap: 5px;
+          background: $color-white-dark;
+          border: 1px solid $color-grey-lite;
+          border-radius: 6px;
+
+          .p-icon {
+            width: 24px;
+          }
+
+          &:hover {
+            border-color: $color-grey-dark;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
