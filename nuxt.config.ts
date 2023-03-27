81ed4dbef5d9ddd7bd0cb8c9ec776887c7a8757b
Edixon Piña <edixonalbertto@gmail.com>
2023-03-27 13:29:37 -0400

    move folder /public in /src and create file .nojekill
    - add baseURL in nuxt.config
    - add file .nojekill to bypassing jekyll and allow the folder /_nuxt
    - move fonts from /static to /public/static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/portfolio/'
+  },
   runtimeConfig: {
     apiSecret: '',
     public: {
